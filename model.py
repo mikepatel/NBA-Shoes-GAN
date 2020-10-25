2cdc40f1ea89f6a3eb8b6de210ba44d36080713e
Michael Patel <michael.patel94@gmail.com>
2020-10-25 00:11:28 -0400

    D will use sequential

--- model.py
+++ model.py
@@ -20,114 +20,12 @@ def build_discriminator():
 
     # Input
     model.add(tf.keras.layers.Input(
-        shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)
+        shape=(, 3)
     ))
-
-    # Conv 64 128x128
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=(3, 3),
-        strides=1,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 64 64x64
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=(3, 3),
-        strides=2,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 64 64x64
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=(3, 3),
-        strides=1,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 64 32x32
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=(3, 3),
-        strides=2,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 128 32x32
-    model.add(tf.keras.layers.Conv2D(
-        filters=128,
-        kernel_size=(3, 3),
-        strides=1,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 128 16x16
-    model.add(tf.keras.layers.Conv2D(
-        filters=128,
-        kernel_size=(3, 3),
-        strides=2,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 256 8x8
-    model.add(tf.keras.layers.Conv2D(
-        filters=256,
-        kernel_size=(3, 3),
-        strides=2,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 512 4x4
-    model.add(tf.keras.layers.Conv2D(
-        filters=512,
-        kernel_size=(3, 3),
-        strides=2,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Conv 512 2x2
-    model.add(tf.keras.layers.Conv2D(
-        filters=512,
-        kernel_size=(3, 3),
-        strides=2,
-        padding="same"
-    ))
-
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
-
-    # Flatten
-    model.add(tf.keras.layers.Flatten())
 
     # Output
     model.add(tf.keras.layers.Dense(
-        units=2
+        activation=tf.keras.activations.sigmoid
     ))
 
     return model
@@ -137,6 +35,10 @@ def build_discriminator():
 # Generator
 # will use skip connections
 def build_generator():
+    # residual block
+    def build_residual_block():
+
+
     # will try Functional API of Model first, otherwise will sub-class Model
 
     # Input
